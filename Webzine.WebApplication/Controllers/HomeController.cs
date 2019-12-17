437fbfe4f8bd0cb760b4f7c692ee375f0831808f
civade nicolas <nicolas.civade@diiage.org>
2019-12-17 08:58:11 +0100

    Mise à jour après intervention client

---
+++ Webzine.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Webzine.WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Return the index page to be displayed
+        /// </summary>
+        /// 
+        /// <returns>Return the index page</returns>
+        /// 
+        [Route("home")]
+        public IActionResult Index()
+        {
+            this.ViewBag.Nom = "Poto";
+            return this.View();
+        }
+    }
+}
--- Webzine.WebApplication/Startup.cs
+++ Webzine.WebApplication/Startup.cs
@@ -16,6 +16,8 @@ namespace Webzine.WebApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,10 @@ namespace Webzine.WebApplication
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=home}/{action=index}"
+                    );
             });
         }
     }
