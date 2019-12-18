0b242d68f424af1981b698fbdc6ef31b5237a00c
Damien Millot <Damien.Millot@diiage.org>
2019-12-18 13:09:40 +0100

    created titre view and titre controller

---
+++ Webzine.WebApplication/Controllers/TitreController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webzine.WebApplication.Controllers
+{
+    public class TitreController : Controller
+    {
+        /// <summary>
+        /// Return the index page to be displayed
+        /// </summary>
+        /// 
+        /// <returns>Return the index page</returns>
+        /// 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
