ecb85c49b8168ab4545edaf74e02f57fd1ef7ce8
Damien Millot <Damien.Millot@diiage.org>
2020-01-07 08:34:11 +0100

    create repositories interfaces

---
+++ Webzine.Repository.Contracts/ITitreRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Webzine.Entity;
+
+namespace Webzine.Repository.Contracts
+{
+    public interface ITitreRepository
+    {
+        void Add(Titre titre);
+        int Count();
+        void Delete(Titre titre);
+        Titre Find(int id);
+        IEnumerable<Titre> FindAll();
+        void IncrementNbLectures(Titre titre);
+        void IncrementNbLikes(Titre titre);
+        IEnumerable<Titre> Search(string mot);
+        IEnumerable<Titre> SearchByStyle(string libelle);
+        void Update(Titre titre);
+    }
+}
