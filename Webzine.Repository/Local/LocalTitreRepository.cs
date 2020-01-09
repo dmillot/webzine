292702aa62d0ae3a0e08b4db67a5187642135b02
Damien Millot <Damien.Millot@diiage.org>
2020-01-09 14:21:53 +0100

    refactoring LocalTitreRepository.cs file to use StyleCop rulesets

--- Webzine.Repository/Local/LocalTitreRepository.cs
+++ Webzine.Repository/Local/LocalTitreRepository.cs
@@ -1,69 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Webzine.Entity;
-using Webzine.Repository.Contracts;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="LocalTitreRepository.cs" company="WebZinc">
+//     Copyright (c) WebZinc. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace Webzine.Repository.Local
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Webzine.Entity;
+    using Webzine.Repository.Contracts;
+
+    /// <summary>
+    /// TO DO.
+    /// </summary>
     public class LocalTitreRepository : ITitreRepository
     {
         /// <summary>
-        /// Permet d'ajouter un style dans notre jeu données
+        /// Méthode pour ajouter un nouveau titre.
         /// </summary>
-        /// <param name="titre"></param>
+        /// <param name="titre">Le titre à ajouter.</param>
         public void Add(Titre titre)
         {
             FactoryTitre.Titres.Add(titre);
         }
 
         /// <summary>
-        /// Permet de récupérer le nombre de titre dans notre jeu de données
+        /// Méthode pour compter le nombre total de titres.
         /// </summary>
-        /// <returns> Retourne le nombre total de Titres</returns>
+        /// <returns>Le nombre total de titres.</returns>
         public int Count() => FactoryTitre.Titres.Count;
-        
+
         /// <summary>
-        /// Permet de supprimer un titre de notre jeu de données
+        /// Méthode pour supprimer un titre.
         /// </summary>
-        /// <param name="titre"></param>
+        /// <param name="titre">Le titre à supprimer.</param>
         public void Delete(Titre titre)
         {
-            FactoryTitre.Titres.Remove(FactoryTitre.Titres.Where(t=>t.IdTitre == titre.IdTitre).FirstOrDefault());
+            FactoryTitre.Titres.Remove(FactoryTitre.Titres.Where(t => t.IdTitre == titre.IdTitre).FirstOrDefault());
         }
 
         /// <summary>
-        /// Permet de trouver un titre précis dans notre jeu de données
+        /// Méthode pour rechercher un titre par son index.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns> Retourne un Titre </returns>
+        /// <param name="id">L'index à chercher.</param>
+        /// <returns>Le titre ayant l'index envoyé.</returns>
         public Titre Find(int id)
         {
             return FactoryTitre.Titres.Where(t => t.IdTitre == id).FirstOrDefault();
         }
+
         /// <summary>
-        /// Permet de récuperer une séquence de titre limitée à partir à partir 
+        /// Méthode pour récupérer les titres demandés (pour la pagination) triés selon la date de création (du plus récent à ancien).
         /// </summary>
-        /// <param name="offset">Nombre d'éléments à ignorer</param>
-        /// <param name="limit">Nombre d'éléments à récupérer</param>
-        /// <returns>Renvoie un nombre de titre définit par la limite</returns>
+        /// <param name="offset">Nombre de titres à ignorer.</param>
+        /// <param name="limit">Nombre de titres à récuperer.</param>
+        /// <returns>La liste des titres demandés triés selon la date de création.</returns>
         public IEnumerable<Titre> FindTitres(int offset, int limit)
         {
             return FactoryTitre.Titres.OrderByDescending(t => t.DateCreation.Date).Skip(offset).Take(limit).ToList();
         }
+
         /// <summary>
-        /// Retourne les titres présents dans notre jeu de données
+        /// Méthode pour récupérer tous les titres.
         /// </summary>
-        /// <returns> Retourne une liste de Titres </returns>
+        /// <returns>La liste de tous les titres.</returns>
         public IEnumerable<Titre> FindAll()
         {
             return FactoryTitre.Titres;
         }
 
         /// <summary>
-        /// Permet d'incrémenter le nombre de lecture d'un titre dans notre jeu de données
+        /// Méthode pour ajouter une vue à un titre.
         /// </summary>
-        /// <param name="titre"></param>
+        /// <param name="titre">Le titre qui gagne une lecture.</param>
         public void IncrementNbLectures(Titre titre)
         {
             int nblectures = FactoryTitre.Titres.Where(t => t == titre).Select(r => r.NbLectures).FirstOrDefault();
@@ -71,9 +81,9 @@ namespace Webzine.Repository.Local
         }
 
         /// <summary>
-        /// Permet d'incrémenter le nombre de likes d'un titre dans notre jeu de données
+        /// Méthode pour ajouter un like à un titre.
         /// </summary>
-        /// <param name="titre"></param>
+        /// <param name="titre">Le titre qui gagne un like.</param>
         public void IncrementNbLikes(Titre titre)
         {
             var nblikes = FactoryTitre.Titres.Where(t => t == titre).Select(r => r.NbLikes).FirstOrDefault();
@@ -81,29 +91,29 @@ namespace Webzine.Repository.Local
         }
 
         /// <summary>
-        /// Permet de rechercher un titre dans notre jeu de données
+        /// Méthode pour chercher les titres dont le nom contient le mot envoyé.
         /// </summary>
-        /// <param name="mot"></param>
-        /// <returns> Retourne une liste de Titres </returns>
+        /// <param name="mot">Le mot à rechercher.</param>
+        /// <returns>La liste des titres dont le nom contient le mot.</returns>
         public IEnumerable<Titre> Search(string mot)
         {
             return FactoryTitre.Titres.FindAll(t => t.Libelle.Contains(mot, StringComparison.OrdinalIgnoreCase));
         }
 
         /// <summary>
-        /// Permet de rechercher un style dans notre jeu de données
+        /// Méthode pour chercher les titres rattachés au style envoyé.
         /// </summary>
-        /// <param name="libelle"></param>
-        /// <returns>c Retourne une liste de Titres </returns>
+        /// <param name="libelle">Le nom du style à rechercher.</param>
+        /// <returns>La liste des titres qui correspondent au style envoyé.</returns>
         public IEnumerable<Titre> SearchByStyle(string libelle)
         {
             return this.FindAll().Where(t => t.TitresStyles.Select(s => s.Libelle).ToString() == libelle).ToList();
         }
 
         /// <summary>
-        /// Permet de de mettre à jour un titre dans notre jeu de données
+        /// Méthode pour modifier un titre.
         /// </summary>
-        /// <param name="titre"></param>
+        /// <param name="titre">Le titre à modifier.</param>
         public void Update(Titre titre)
         {
             var rank = 0;
@@ -116,14 +126,16 @@ namespace Webzine.Repository.Local
                     FactoryTitre.Titres[rank] = titre;
                     break;
                 }
+
                 rank++;
             }
         }
+
         /// <summary>
-        /// Permet de récupérer les 3 titres les plus aimés à partir d'une certaine date
+        /// Méthode pour récupérer les titres les plus populaires qui ont été crée entre la date envoyée et maintenant.
         /// </summary>
-        /// <param name="dateRecherche">La date après laquelle on souhaite récuperer nos titres</param>
-        /// <returns>Retourne une liste de 3 titres classé du plus aimé au moins aimé</returns>
+        /// <param name="dateRecherche">La date à ne pas dépasser dans la recherche.</param>
+        /// <returns>La liste des titres les plus populaires.</returns>
         public IEnumerable<Titre> GetPopular(DateTime dateRecherche)
         {
             return FactoryTitre.Titres.OrderByDescending(t => t.NbLikes).Where(r => r.DateCreation > dateRecherche).Take(3).ToList();
