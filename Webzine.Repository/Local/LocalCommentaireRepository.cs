50f94296a6d5e526a58d7e6802a74c34e787e6ec
Damien Millot <Damien.Millot@diiage.org>
2020-01-09 14:18:24 +0100

    refactoring LocalCommentaireRepository.cs file to use StyleCop rulesets

--- Webzine.Repository/Local/LocalCommentaireRepository.cs
+++ Webzine.Repository/Local/LocalCommentaireRepository.cs
@@ -1,25 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Webzine.Entity;
-using Webzine.Repository.Contracts;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="LocalCommentaireRepository.cs" company="WebZinc">
+//     Copyright (c) WebZinc. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace Webzine.Repository.Local
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Webzine.Entity;
+    using Webzine.Repository.Contracts;
+
+    /// <summary>
+    /// TO DO.
+    /// </summary>
     public class LocalCommentaireRepository : ICommentaireRepository
     {
+        /// <summary>
+        /// Méthode pour ajouter un nouveau commentaire.
+        /// </summary>
+        /// <param name="commentaire">Le commentaire à ajouter.</param>
         public void Add(Commentaire commentaire)
         {
             commentaire.Titre = FactoryTitre.Titres.FirstOrDefault(t => t.IdTitre == commentaire.IdTitre);
             FactoryTitre.Titres.First(t => t.IdTitre == commentaire.IdTitre).Commentaires.Add(commentaire);
         }
 
+        /// <summary>
+        /// Méthode pour supprimer un commentaire.
+        /// </summary>
+        /// <param name="commentaire">Le commentaire à supprimer.</param>
         public void Delete(Commentaire commentaire)
         {
-            FactoryTitre.Titres.First(t => t.IdTitre == commentaire.IdTitre).Commentaires.RemoveAll(c=>c.IdCommentaire == commentaire.IdCommentaire);
+            FactoryTitre.Titres.First(t => t.IdTitre == commentaire.IdTitre).Commentaires.RemoveAll(c => c.IdCommentaire == commentaire.IdCommentaire);
         }
 
+        /// <summary>
+        /// Méthode pour rechercher un commentaire par son index.
+        /// </summary>
+        /// <param name="id">L'index à chercher.</param>
+        /// <returns>Le commentaire ayant l'index envoyé.</returns>
         public Commentaire Find(int id)
         {
             Commentaire commentaire = FactoryCommentaire.Commentaires.FirstOrDefault(c => c.IdCommentaire == id);
@@ -27,6 +46,10 @@ namespace Webzine.Repository.Local
             return commentaire;
         }
 
+        /// <summary>
+        /// Méthode pour récupérer tous les commentaires.
+        /// </summary>
+        /// <returns>La liste de tous les commentaires.</returns>
         public IEnumerable<Commentaire> FindAll()
         {
             FactoryCommentaire.Commentaires.ForEach(c => c.Titre = FactoryTitre.Titres.First(t => t.IdTitre == c.IdTitre));
