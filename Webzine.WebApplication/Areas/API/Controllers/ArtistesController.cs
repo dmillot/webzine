f2af0859dffe1a253997c32e3e7aade8a54942c8
Kim Dole <Kim.dole@diiage.org>
2020-01-15 16:36:31 +0100

    WebApi finished

---
+++ Webzine.WebApplication/Areas/API/Controllers/ArtistesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Webzine.Repository.Contracts;
+
+namespace Webzine.WebApplication.Areas.API.Controllers
+{
+    
+    [ApiController]
+    public class ArtistesController : ControllerBase
+    {
+        IArtisteRepository _artisteRepository;
+
+        public ArtistesController(IArtisteRepository artisteRepository)
+        {
+            this._artisteRepository = artisteRepository;
+        }
+
+        /// <summary>
+        /// Permet de récupérer tous les artistes.
+        /// </summary>
+        /// <returns>La liste des artistes.</returns>
+        [HttpGet]
+        [Route("api/artistes")]
+        public IActionResult GetArtiste()
+        {
+            try
+            {
+                var result = JsonConvert.SerializeObject(_artisteRepository.FindAll(), Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+    }
+}
--- Webzine.WebApplication/Components/StylesList.cs
+++ Webzine.WebApplication/Components/StylesList.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Webzine.Entity;
-using Webzine.Repository;
 using Webzine.Repository.Contracts;
 
 namespace Webzine.WebApplication
@@ -13,10 +9,12 @@ namespace Webzine.WebApplication
     public class StylesList : ViewComponent
     {
         private readonly IStyleRepository _styleRepository;
+
         public StylesList(IStyleRepository styleRepository)
         {
             _styleRepository = styleRepository;
         }
+
         public IViewComponentResult Invoke()
         {
             List<Style> styles = (List<Style>)_styleRepository.FindAll();
